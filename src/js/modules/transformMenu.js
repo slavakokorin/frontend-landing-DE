db21fbd5eca0f27283747f3a9c6d206f49965631
Vyacheslav <kokorinv19@gmail.com>
2022-04-25 09:03:41 +0500

    modified

--- src/js/modules/transformMenu.js
+++ src/js/modules/transformMenu.js
@@ -1,17 +1,19 @@
-export default () => {
-  window.onscroll = function() {
+import { throttle } from "./functions.js";
+
+function transformMenu() {
+  window.onscroll = throttle(function() {
     const scrolled = window.pageYOffset || document.documentElement.scrollTop;
     if(scrolled > 200){
       document.querySelector('.header__top--fixed').classList.add('bg--gray-blue');
       document.querySelector('.burger__button').classList.add('bg--gray-blue');
-      document.querySelector('.menu__nav').classList.add('bg--gray-blue');
       document.querySelector('.header__top--fixed').classList.add('header__top--min');
     }
     else if (scrolled < 200) {
       document.querySelector('.header__top--fixed').classList.remove('bg--gray-blue');
       document.querySelector('.burger__button').classList.remove('bg--gray-blue');
-      document.querySelector('.menu__nav').classList.remove('bg--gray-blue');
       document.querySelector('.header__top--fixed').classList.remove('header__top--min');      
     }
-  }
+  }, 300)
 }
+
+export default transformMenu;
